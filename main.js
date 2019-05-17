422bb70457294d679f5e0678f2e7fbe71a123a67
Dan Ross <daniellross@gmail.com>
2019-05-17 15:28:59 -0500

    editing js to fill html with div tags

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-ht
+window.onload = fetch('https://randomuser.me/api/?results=10').then(function(response){
+    return response.json();
+    }
+).then(
+    function(data){
+        console.log(data);
+        for (let x = 0; x < data.results.length; x++){
+            document.getElementById("wanted").innerHTML = 
+            "<div id='" + "crim" + x + ">" + "<div id='";
+        }
+    }
+)
